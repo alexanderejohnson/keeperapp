7d9f01d7523d08ad97d90a5d2bfc2a569051b1db
Alex <ajohnson2389@gmail.com>
2020-02-04 19:49:32 -0500

    initial commit with react

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+        <p>Copyright © {new Date().getFullYear()} Alex Johnson</p>
+        </footer>
+
+    );
+
+}
+
+export default Footer;
