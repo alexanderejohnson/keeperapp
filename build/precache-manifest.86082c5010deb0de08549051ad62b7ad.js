d4110fabf7ad6cb7503e286bf074af763978b86d
Alex <ajohnson2389@gmail.com>
2020-02-19 11:18:33 -0500

    initial commit

---
+++ build/precache-manifest.86082c5010deb0de08549051ad62b7ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca7e39f02ab9236f7d3d3aff05face6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc9b96364138490141d5",
+    "url": "/static/js/2.87b7df72.chunk.js"
+  },
+  {
+    "revision": "cbfd40ecff8f43ec45375d468dd1abc8",
+    "url": "/static/js/2.87b7df72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cece12e37ad18b6db49",
+    "url": "/static/js/main.fea42f0f.chunk.js"
+  },
+  {
+    "revision": "ba68b8dcb7b7d5fe69ba",
+    "url": "/static/js/runtime-main.5ae4c87f.js"
+  }
+]);
